f7396002e91ecaaab3fa1b57b98a75cc6c049fb7
leeozaka <leonardoanicetoneves@hotmail.com>
2025-05-05 18:00:07 -0300

    refactor: update user creation and retrieval methods to use CreateUserDTO

--- cachorrinho-express/src/interfaces/UserInterface.ts
+++ cachorrinho-express/src/interfaces/UserInterface.ts
@@ -1,11 +1,4 @@
-import { User } from 'dtos/UserDTO';
-
-export interface CreateUserRequest {
-  cpf: string;
-  password: string;
-  email: string;
-  telephone: string;
-}
+import { CreateUserDTO, User } from 'dtos/UserDTO';
 
 export enum UserRole {
   ADMIN = 'ADMIN',
@@ -13,7 +6,7 @@ export enum UserRole {
 }
 
 export interface IUserRepository {
-  create(data: User): Promise<User>;
+  create(data: CreateUserDTO): Promise<User>;
   findOne(id: string): Promise<User>;
   findAll(filter?: Partial<User>): Promise<User[]>;
   update(id: string, data: Partial<User>): Promise<User>;
@@ -22,7 +15,7 @@ export interface IUserRepository {
 }
 
 export interface IUserService {
-  create(data: CreateUserRequest): Promise<User>;
+  create(data: CreateUserDTO): Promise<User>;
   findOne(id: string): Promise<User>;
   findAll(filter?: Partial<User>): Promise<User[]>;
   update(id: string, data: Partial<User>): Promise<User>;
